dbe26c206385fff73f147a8e4d315a02f73ddcbb
jimcris <jimcriz23david@gmail.com>
2024-12-02 23:36:21 +0800

    hands-on2

---
+++ hands-on2/index.php
@@ -0,0 +1,13 @@
+<?php
+// single line comment
+# single line comment
+/* multi line comment */
+echo "<h1> hello world </h1>";
+$firstName = "Adrian";
+$lastName = "Mendoza";
+echo "<h3>hello " . $firstName . " " . $lastName . "</h3>";
+
+$x = 10;
+$y = 99.9;
+$z = $x * $y;
+echo $z;    
